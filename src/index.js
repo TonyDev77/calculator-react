0a93499cd8a54e01dd2f6e4e1f71a112a500e6b2
TonySilva7 <tony.deve7oper@gmail.com>
2021-03-18 12:23:23 -0300

    Normalized the template of fonts, dimensions and colors

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import './main/Calculator'
 import reportWebVitals from './reportWebVitals';
+import Calculator from "./main/Calculator";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+      <h1> Calculadora </h1>
+      <Calculator />
   </React.StrictMode>,
   document.getElementById('root')
 );
